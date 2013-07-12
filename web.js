c7880ff1f7dfae8dbb5ba89e76d447c8633a6666
James Stephenson <jamesst013@yahoo.com>
2013-07-12 21:43:36 +0000

    fixes

--- web.js
+++ web.js
@@ -8,8 +8,8 @@ app.get('/', function(request, response) {
   fs.readFile('/index.html', function (err, data) {
     if (err) throw err;
     response.send(data).toString();
-  }  
-  
+  });
+
 });
 
 var port = process.env.PORT || 5000;
